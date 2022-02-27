74a5e0bfa70a69c8c4c2c9a3d1a68847aab32703
Glen Langer <contao.modul@glen-langer.de>
2022-02-27 15:51:54 +0100

    update copyright

--- src/EventListener/ExportPanelListener.php
+++ src/EventListener/ExportPanelListener.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 /*
  * This file is part of a BugBuster Contao Bundle
- * @copyright  Glen Langer 2008..2020 <http://contao.ninja>
+ * @copyright  Glen Langer 2008..2022 <http://contao.ninja>
  * @author     Glen Langer (BugBuster)
  * @author     Alexander Kehr (Kehr-Solutions) <https://www.kehr-solutions.de>
  * @license    LGPL-3.0-or-later
