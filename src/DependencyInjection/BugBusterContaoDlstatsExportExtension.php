6cd45f0ac2341eb23ad0efd01683ee8d23694aef
Glen Langer <contao.modul@glen-langer.de>
2023-12-23 23:06:10 +0100

    #18, try to fix install error

--- src/DependencyInjection/BugBusterContaoDlstatsExportExtension.php
+++ src/DependencyInjection/BugBusterContaoDlstatsExportExtension.php
@@ -15,10 +15,10 @@ namespace BugBuster\DlstatsExportBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
+//use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class BugBusterContaoDlstatsExportExtension extends Extension
+class BugBusterContaoDlstatsExportExtension extends \Symfony\Component\DependencyInjection\Extension\ExtensionInterface //Extension
 {
     /**
      * {@inheritdoc}
