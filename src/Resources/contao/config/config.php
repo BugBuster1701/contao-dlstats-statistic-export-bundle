80d4a84aeae8d7bfa1c0104d9ce96d6fc2ad5f36
Glen Langer <contao.modul@glen-langer.de>
2023-12-24 01:01:50 +0100

    #18, run composer ecs-legacy

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,4 +1,15 @@
 <?php
 
-// Hooks
-$GLOBALS['TL_DLSTATS_HOOKS']['addStatisticPanelLine'][] = ['bugbuster.dlstatsexport.listener.panel', 'onGetPanelLine'];
+/*
+ * This file is part of a BugBuster Contao Bundle.
+ *
+ * @copyright  Glen Langer 2023 <http://contao.ninja>
+ * @author     Glen Langer (BugBuster)
+ * @author     Alexander Kehr (Kehr-Solutions)
+ * @package    Contao Download Statistics Bundle (Dlstats) Add-on: Statistic Export
+ * @link       https://github.com/BugBuster1701/contao-dlstats-statistic-export-bundle
+ *
+ * @license    LGPL-3.0-or-later
+ */
+
+$GLOBALS['TL_DLSTATS_HOOKS']['addStatisticPanelLine'][] = array('bugbuster.dlstatsexport.listener.panel', 'onGetPanelLine');
